c4e4cfae239ba51459f222aacf4f32999197dc51
Robin Hayman <r.hayman@ucl.ac.uk>
2022-07-14 17:13:38 +0100

    added args to allow export of different bits of tiddler

--- tiddly2md.py
+++ tiddly2md.py
@@ -2,11 +2,13 @@
 # coding: utf-8
 
 from __future__ import print_function
+
 import argparse
-import re
 import os
-import unicodedata
+import re
 import sys
+import unicodedata
+from queue import Empty
 
 import pandas as pd
 
@@ -33,13 +35,13 @@ def wiki_to_md(text):
     fn = []
     new_text = []
     fn_n = 1  # Counter for footnotes
-    for line in text.split('\n'):
+    for line in text.split("\n"):
         # Replace wiki headers with markdown headers
-        match = re.match('(!+)(\\s?)[^\\[]', line)
+        match = re.match("(!+)(\\s?)[^\\[]", line)
         if match:
             header, spaces = match.groups()
-            new_str = '#' * len(header)
-            line = re.sub('(!+)(\\s?)([^\\[])', new_str + ' ' + '\\3', line)
+            new_str = "#" * len(header)
+            line = re.sub("(!+)(\\s?)([^\\[])", new_str + " " + "\\3", line)
         # Underline (doesn't exist in MD)
         line = re.sub("__(.*?)__", "\\1", line)
         # Bold
@@ -57,14 +59,14 @@ def wiki_to_md(text):
         if match:
             text = match.groups()[0]
             fn.append(text)
-            line = re.sub("```(.*)```", '[^{}]'.format(fn_n), line)
+            line = re.sub("```(.*)```", "[^{}]".format(fn_n), line)
             fn_n += 1
         new_text.append(line)
 
     # Append footnotes
     for i, each in enumerate(fn):
-        new_text.append('[^{}]: {}'.format(i+1, each))
-    return '\n'.join(new_text)
+        new_text.append("[^{}]: {}".format(i + 1, each))
+    return "\n".join(new_text)
 
 
 def sanitize(value):
@@ -73,8 +75,8 @@ def sanitize(value):
     :param value: string
     """
     value = str(value)
-    value = unicodedata.normalize('NFKD', value)
-    value = str(re.sub('[^\w\s-]', '', value).strip())
+    value = unicodedata.normalize("NFKD", value)
+    value = str(re.sub("[^\w\s-]", "", value).strip())
     return value
 
 
@@ -92,29 +94,68 @@ def main(args):
 
     for row_id, row in df.iterrows():
         filename = sanitize(row.title)
-        filename = '{}.{}'.format(filename, args.ext)
-        with open(os.path.join(output_path, filename), 'w') as f:
-            try:
-                f.write(wiki_to_md(row.text))
-            except:
-                f.write('')
+        if args.sections:
+            for rec_type in args.sections:
+                fname = filename + "_" + rec_type
+                fname = "{}.{}".format(fname, args.ext)
+                txt = row.text
+                if len(txt) > 1:
+                    print(f"Filename: {fname}")
+                    if "surgery" in rec_type:
+                        txt = txt.split("!!!")[1]
+                    if "screening" in rec_type:
+                        txt = txt.split("!!!")[2:]
+                    txt = "".join(txt)
+                    txt = "!!!" + txt
+                    with open(os.path.join(output_path, fname), "w") as f:
+                        try:
+                            f.write(wiki_to_md(txt))
+                        except:
+                            f.write("")
+        else:
+            fname = "{}.{}".format(fname, args.ext)
+            with open(os.path.join(output_path, fname), "w") as f:
+                try:
+                    f.write(wiki_to_md(row.text))
+                except:
+                    f.write("")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Convert TiddlyWiki tiddlers exported as CSV to "
-                    "individual files.")
-    parser.add_argument('--ext', '-e', dest='ext',
-                        default='md',
-                        help='File extension (defaults to "md")')
-    parser.add_argument('--outdir', '-o',
-                        default='output',
-                        dest='outdir',
-                        help='Output folder (defaults to "output")')
-    parser.add_argument('--tags', '-t', dest='tags',
-                        action='append',
-                        help='Valid tag to export, can have multiple')
-    parser.add_argument('input_file',
-                        help='Exported CSV file')
+        "individual files."
+    )
+    parser.add_argument(
+        "--ext",
+        "-e",
+        dest="ext",
+        default="md",
+        help='File extension (defaults to "md")',
+    )
+    parser.add_argument(
+        "--outdir",
+        "-o",
+        default="output",
+        dest="outdir",
+        help='Output folder (defaults to "output")',
+    )
+    parser.add_argument(
+        "--tags",
+        "-t",
+        dest="tags",
+        action="append",
+        help="Valid tag to export, can have multiple",
+    )
+    parser.add_argument("input_file", help="Exported CSV file")
+    parser.add_argument(
+        "--sections",
+        "-s",
+        dest="sections",
+        action="append",
+        help="Which section to extract",
+    )
+    parser.add_argument("--screening", help="Extract only the screening section")
     args = parser.parse_args()
 
     sys.exit(main(args))
